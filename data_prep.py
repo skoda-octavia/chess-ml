3eefcf964d1e6218030cf0be26a09d8aa0b55ac5
Piotr pc <Piotr.kow.59@gmail.com>
2024-03-18 00:12:48 +0100

    data for pieces prepared

--- data_prep.py
+++ data_prep.py
@@ -1,12 +1,11 @@
+import chess
 import chess.pgn
 import torch
-import torch.nn as nn
-import torch.optim as optim
-import matplotlib.pyplot as plt
 
 def board_to_matrix(board: chess.Board, move: chess.Move):
     matrix_board = torch.zeros((6, 8, 8))
-    matrix_move = torch.zeros((8, 8))
+    matrix_move_from = torch.zeros((8, 8))
+    matrix_move_to = torch.zeros((8, 8))
 
     for i in range(8):
         for j in range(8):
@@ -15,96 +14,69 @@ def board_to_matrix(board: chess.Board, move: chess.Move):
                 piece_type = piece.piece_type
                 piece_color = piece.color
                 index = piece_type - 1
+                y_idx = 7 - j if board.turn == chess.WHITE else j
+                # x_idx = i if board.turn == chess.WHITE else 7 - i
                 if piece_color == chess.WHITE:
-                    matrix_board[index, 7-j, i] = 1
+                    matrix_board[index, y_idx, i] = 1
                 else:
-                    matrix_board[index, 7-j, i] = -1
+                    matrix_board[index, y_idx, i] = -1
+    
                 # print(matrix_board)
+                    
+    file_from = chess.square_file(move.from_square)
+    rank_from = chess.square_rank(move.from_square)
+    file_to = chess.square_file(move.to_square)
+    rank_to = chess.square_rank(move.to_square)
+    matrix_move_from[7-rank_from][file_from] = 1
+    matrix_move_to[7-rank_to][file_to] = 1
+
     if board.turn == chess.BLACK:
         matrix_board *= -1
+        matrix_move_from = torch.flip(matrix_move_from, dims=[0])
+        matrix_move_to = torch.flip(matrix_move_to, dims=[0])
 
-    file = chess.square_file(move.from_square)
-    rank = chess.square_rank(move.from_square)
-    matrix_move[7-rank][file] = 1
-    # print(matrix_board)
-    return matrix_board.tolist(), matrix_move.tolist()
+    matrix_move = torch.cat((matrix_board, matrix_move_from.unsqueeze(0)), 0),
+    return matrix_board, matrix_move_from, matrix_move[0], matrix_move_to, board.piece_at(move.from_square).piece_type
 
 def main():
     pgn = open("full.pgn")
     cnt = 0
-    board_matrix, piece_matrix = [], []
+    board_data, piece_from_data = [], []
+    piece_moves_dict = {
+        chess.PAWN : [[], []],
+        chess.KNIGHT : [[], []],
+        chess.BISHOP : [[], []],
+        chess.ROOK : [[], []],
+        chess.QUEEN : [[], []],
+        chess.KING : [[], []]
+    }
 
     while True:
         game = chess.pgn.read_game(pgn)
         if game is None:
             break
         cnt+=1
-        if cnt % 100 == 0:
+        if cnt % 500 == 0:
             print(cnt)
-            
+
         board = game.board()
         for move in game.mainline_moves():
-            if board.turn == chess.WHITE:
-                matrix_board, matrix_move = board_to_matrix(board, move)
-                board_matrix.append(matrix_board)
-                piece_matrix.append(matrix_move)
+            board_matrix, piece_from_matrix, matrix_move, matrix_move_to, piece_type  = board_to_matrix(board, move)
+            piece_moves_dict[piece_type][0].append(matrix_move.tolist())
+            piece_moves_dict[piece_type][1].append(matrix_move_to.tolist())
+            board_data.append(board_matrix.tolist())
+            piece_from_data.append(piece_from_matrix.tolist())
             board.push(move)
 
 
-    X = torch.tensor(board_matrix)
-    y = torch.tensor(piece_matrix)
-    torch.save(X, "X.pt")
-    torch.save(y, "y.pt")
+    X = torch.tensor(board_data)
+    y = torch.tensor(piece_from_data)
+    for piece, list in piece_moves_dict.items():
+        tensor_data = torch.tensor(list[0])
+        tensor_y = torch.tensor(list[1])
+        torch.save(tensor_data, f"X_{chess.piece_name(piece)}.pt")
+        torch.save(tensor_y, f"Y_{chess.piece_name(piece)}.pt")
+    torch.save(X, "X_all.pt")
+    torch.save(y, "y_all.pt")
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print(device)
-
-
-def test():
-    X = torch.load("X.pt")
-    y = torch.load("y.pt")
-    assert len(X) == len(y), "Długość danych wejściowych i etykiet musi być taka sama"
-
-    model = nn.Sequential(
-        nn.Flatten(),
-        nn.Linear(8*8*6, 64),
-        nn.ReLU(),
-        nn.Linear(64, 32),
-        nn.ReLU(),
-        nn.Linear(32, 16),
-        nn.ReLU(),
-        nn.Linear(16, 8*8),
-    )
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print(torch.cuda.is_available())
-    print(device)
-    X.to(device)
-    y.to(device)
-    model.to(device)
-
-    criterion = nn.MSELoss()
-    optimizer = optim.SGD(model.parameters(), lr=0.01)
-    num_epochs = 1000
-    eps = []
-    losses = []
-
-    for epoch in range(num_epochs):
-        optimizer.zero_grad()
-        output = model(X.view(-1, 8*8*6))
-        loss = criterion(output, y.view(-1, 8*8))
-        loss.backward()
-        optimizer.step()
-        if (epoch+1) % 10 == 0:
-            print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
-            eps.append(epoch)
-            losses.append(loss.item())
-
-    plt.plot(eps, losses)
-    plt.xlabel("eps")
-    plt.ylabel("loss")
-    plt.show()
-
-
-if __name__ == "__main__":
-    # main()
-    test()
+main()
---
+++ plotting.py
@@ -0,0 +1,28 @@
+from matplotlib import pyplot as plt
+# Epoch [14000/150000], train_acc: 0.089935, test_acc: 0.087635
+import torch
+
+def generate(filename: str):
+    eps = []
+    val_accs = []
+    train_accs = []
+    with open(filename, "r") as file:
+        for line in file:
+            words = line.split(',')
+            train_acc_val = float(words[1].split(':')[1])
+            test_acc_val = float(words[2].split(':')[1])
+            eps_line = words[0].split(' ')
+            eps_num = eps_line[1][1:].split('/')[0]
+            eps.append(int(eps_num))
+            val_accs.append(test_acc_val)
+            train_accs.append(train_acc_val)
+    plt.plot(eps, train_accs, label="train accs")
+    plt.plot(eps, val_accs, label="val accs")
+    plt.legend()
+    plt.xlabel("eps")
+    plt.ylabel("accuracy")
+    plt.title(f"Accuracy, leraning rate: {filename.split('.')[0].split('-')[0]}")
+    plt.show()
+
+# generate("004-100000.txt")
+print(torch.cuda.is_available())
