8164a9cd54947ed02c06fdac652156a51372542e
piotras <piotr.kow.59@gmail.com>
2024-07-21 11:42:34 +0200

    move prediction to cuda

--- src/rl/monte.py
+++ src/rl/monte.py
@@ -2,6 +2,7 @@ import torch.optim as optim
 import random
 from game import Game
 import torch
+import chess
 
 
 def record(game, score, model, optimalizer, lock):
@@ -10,50 +11,53 @@ def record(game, score, model, optimalizer, lock):
 def heuristic_value(tensor, model):
     return model.predict(tensor)
 
-def get_move(action_dict: dict, white_moves):
+def get_move(heus: torch.Tensor, moves: list[chess.Move], white_moves, to_consider=3):
     const_bias = 0.05
-    if len(action_dict) == 1:
-        return list(action_dict.keys())[0]
-    moves = list(action_dict.keys())
-    values = list(action_dict.values())
+    heus = torch.squeeze(heus)
+    if len(moves) == 1:
+        return moves[0]
     if not white_moves:
-        values = [-val for val in values]
-    bias = min(values)
+        heus = heus * -1
+    bias = torch.min(heus)
     if bias < 0:
-        values = [(val+abs(bias)+const_bias) for val in values]
-    try:
-        choice = random.choices(values, weights=values, k=1)
-    except ValueError:
-        print(values)
-        print(white_moves)
-        print(action_dict)
-        raise ValueError
-    return moves[values.index(choice[0])]
+        heus -= bias
+    topk_values, topk_indices = torch.topk(heus, to_consider)
+    probabilities = topk_values / torch.sum(topk_values)
+
+    chosen_index = torch.multinomial(probabilities, 1)
+    selected_index = topk_indices[chosen_index]
+    return moves[selected_index.item()]
 
 
-def playout_value(game: Game, model, optimalizer, lock):
+def playout_value(game: Game, model, optimalizer, lock, device):
     if game.over():
+        print(game.board)
+        print(f"---------------{game.score()}")
         score = torch.tensor(game.score())
         record(game, score, model, optimalizer, lock)
         return torch.tensor(score.item())
 
-    action_heuristic_dict = {}
+    next_states = []
+    moves = []
     for move in game.valid_moves():
+        moves.append(move)
         tempTensor = game.simulate_move(move)
-        heu = heuristic_value(tempTensor, model).item()
-        action_heuristic_dict[move] = heu
-    move = get_move(action_heuristic_dict, game.board.turn)
+        next_states.append(tempTensor)
+
+    final = torch.stack(next_states).to(device)
+    heus = model.predict(final)
+    move = get_move(heus, moves, game.board.turn)
 
     next_game = game.copy()
     next_game.make_move(move)
 
-    value = playout_value(next_game, model, optimalizer, lock)
+    value = playout_value(next_game, model, optimalizer, lock, device)
     record(game, value, model, optimalizer, lock)
 
     return value
 
-def monte_carlo_value(game, N, model, optimalizer, lock):
+def monte_carlo_value(game, N, model, optimalizer, lock, device):
     res = []
     for _ in range(N):
-        res.append(playout_value(game, model, optimalizer, lock))
+        res.append(playout_value(game, model, optimalizer, lock, device))
     return res
