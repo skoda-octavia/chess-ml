ea44cd190a1225a4d7e04dbc37f2e1f8c193b64a
piotras <piotr.kow.59@gmail.com>
2024-08-18 12:29:35 +0200

    lstm acc added

--- src/lstm/seq2seq.py
+++ src/lstm/seq2seq.py
@@ -7,7 +7,14 @@ import torch.optim as optim
 import torch.nn.functional as F
 import pandas as pd
 import random
-from seqModules import Seq2Seq
+from seqModules import Seq2Seq, SequenceDataset
+from utils import (
+    get_samples,
+    translate_data,
+    get_valid_accs,
+    load_data,
+    find_max_length
+    )
 
 seed = 42
 
@@ -16,46 +23,8 @@ torch.manual_seed(seed)
 torch.cuda.manual_seed(seed)
 torch.backends.cudnn.deterministic = True
 
-class SequenceDataset(Dataset):
-    def __init__(self, src_sequences, tar_sequences, src_padd_idx, tar_padd_idx, max_src_len, max_tar_len):
-        self.src_sequences = [torch.tensor(seq) for seq in src_sequences]
-        self.tar_sequences = [torch.tensor(seq) for seq in tar_sequences]
-        # self.src_padd_idx = src_padd_idx
-        # self.tar_padd_idx = tar_padd_idx
-        self.max_src_len = max_src_len
-        self.max_tar_len = max_tar_len
 
-    def __len__(self):
-        return len(self.src_sequences)
-
-    def __getitem__(self, idx):
-        src_seq = self.src_sequences[idx]
-        tar_seq = self.tar_sequences[idx]
-
-        # src_seq = torch.nn.functional.pad(src_seq, (0, self.max_src_len - len(src_seq)), value=self.src_padd_idx)
-        # tar_seq = torch.nn.functional.pad(tar_seq, (0, self.max_tar_len - len(tar_seq)), value=self.tar_padd_idx)
-
-        return src_seq, tar_seq
-
-
-def load_data(csv_path, test_size=0.1, random_state=42):
-    df = pd.read_csv(csv_path)
-    df = df.sample(frac=0.4, random_state=random_state).reset_index(drop=True)
-
-    sequences = df['sequence'].apply(json.loads).tolist()
-    targets = df['target'].apply(json.loads).tolist()
-
-    seq_train, seq_val, tar_train, tar_val = train_test_split(
-        sequences, targets, test_size=test_size, random_state=random_state
-    )
-
-    return seq_train, seq_val, tar_train, tar_val
-
-def find_max_length(sequences):
-    return max(len(seq) for seq in sequences)
-
-
-def validate(model: nn.Module, val_dl, criterion, device):
+def validate(model: nn.Module, val_dl, criterion, device, merged_data, merged_targs):
 
         loss_sum = 0
         model.eval()
@@ -72,7 +41,14 @@ def validate(model: nn.Module, val_dl, criterion, device):
             loss = criterion(output, tar)
             loss_sum += loss.item()
 
-        return loss_sum / len(val_dl)
+        # acc
+        seq = merged_data.permute(1, 0)
+        tar = merged_targs.permute(1, 0)
+        output = model(seq, tar, 0)
+        output = output.permute(1, 0, 2)
+        output = torch.argmax(output, -1)
+        acc, corr = get_valid_accs(output, sample_games_data, tar_vocab)
+        return loss_sum / len(val_dl), acc, corr
 
 
 def train(model, train_dl, opti, criterion, device):
@@ -99,7 +75,7 @@ def train(model, train_dl, opti, criterion, device):
 
 
 
-def fit(model, train_dl, val_dl, eps, opti, criterion, device):
+def fit(model, train_dl, val_dl, eps, opti, criterion, merged_data, merged_targs, device):
 
     train_losses, val_losses = [], []
     for i in range(eps):
@@ -107,10 +83,12 @@ def fit(model, train_dl, val_dl, eps, opti, criterion, device):
         train_loss = train(model, train_dl, opti, criterion, device)
         train_losses.append(train_losses)
         
-        val_loss = validate(model, val_dl, criterion, device)
+        val_loss, acc, corr = validate(model, val_dl, criterion, device, merged_data, merged_targs)
         val_losses.append(val_loss)
         
-        print(f"eps: {i}, train loss: {train_loss:.4f}, val loss: {val_loss:.4f}")
+        print(f"eps: {i}, train loss: {train_loss:.4f}, val loss: {val_loss:.4f}, acc: {acc:.4f}, legal: {corr:.4f}")
+        torch.save(model.state_dict(), f"transformer{i}.pth")
+        torch.save(opti.state_dict(), f"opti{i}.pth")
 
 vocab_path = "src/lstm/fen_vocab_src.json"
 tar_vocab_path = "src/lstm/vocab_tar.json"
@@ -122,10 +100,14 @@ with open(tar_vocab_path, "r") as f:
 csv_path = 'data/prep/tokenized_ones.csv'
 src_vocab_len = len(vocab)
 tar_vocab_len = len(tar_vocab)
+
+# data params
 batch=512
 num_workers=32
+frac = 0.002
+valid_size = 0.2
 
-seq_train, seq_val, tar_train, tar_val = load_data(csv_path)
+seq_train, seq_val, tar_train, tar_val = load_data(csv_path, frac=frac, test_size=valid_size)
 
 max_src_len = max(find_max_length(seq_train), find_max_length(seq_val))
 max_tar_len = max(find_max_length(tar_train), find_max_length(tar_val))
@@ -136,20 +118,30 @@ dataset_val = SequenceDataset(seq_val, tar_val, src_vocab_len, tar_vocab_len, ma
 dataloader_train = DataLoader(dataset_train, batch_size=batch, shuffle=True, drop_last=True, num_workers=num_workers)
 dataloader_val = DataLoader(dataset_val, batch_size=batch, shuffle=True, drop_last=True, num_workers=num_workers)
 
+print(f"train len: {len(dataloader_train)}")
+print(f"val len: {len(dataloader_val)}\n")
+
+# train params
 eps= 30
 lr = 0.001
 device = torch.device("cuda")
 embed = 512
-hidden = 1024
-layers = 6
-drop = 0.1
+hidden = 512
+layers = 4
+drop = 0.2
 
-print(f"train len: {len(dataloader_train)}")
-print(f"val len: {len(dataloader_val)}\n")
+# acc validation params
+samples = 10240
+
+merged_data, merged_targs = get_samples(dataloader=dataloader_val,samples=10240)
+sample_games_data = translate_data(merged_data, merged_targs, vocab, tar_vocab)
+merged_data, merged_targs = merged_data.to(device), merged_targs.to(device)
+
+print(f"accuracy check data len: {len(merged_data)}\n")
 
 model = Seq2Seq(src_vocab_len, tar_vocab_len, embed, hidden, layers, drop).to(device)
 
 criterion = nn.CrossEntropyLoss()
 opti = optim.Adam(model.parameters(), lr=lr)
 
-fit(model, dataloader_train, dataloader_val, eps, opti, criterion, device)
+fit(model, dataloader_train, dataloader_val, eps, opti, criterion, merged_data, merged_targs, device)
--- src/lstm/seqModules.py
+++ src/lstm/seqModules.py
@@ -1,7 +1,28 @@
 import torch
 import torch.nn as nn
 import random
+from torch.utils.data import DataLoader, Dataset
 
+class SequenceDataset(Dataset):
+    def __init__(self, src_sequences, tar_sequences, src_padd_idx, tar_padd_idx, max_src_len, max_tar_len):
+        self.src_sequences = [torch.tensor(seq) for seq in src_sequences]
+        self.tar_sequences = [torch.tensor(seq) for seq in tar_sequences]
+        # self.src_padd_idx = src_padd_idx
+        # self.tar_padd_idx = tar_padd_idx
+        self.max_src_len = max_src_len
+        self.max_tar_len = max_tar_len
+
+    def __len__(self):
+        return len(self.src_sequences)
+
+    def __getitem__(self, idx):
+        src_seq = self.src_sequences[idx]
+        tar_seq = self.tar_sequences[idx]
+
+        # src_seq = torch.nn.functional.pad(src_seq, (0, self.max_src_len - len(src_seq)), value=self.src_padd_idx)
+        # tar_seq = torch.nn.functional.pad(tar_seq, (0, self.max_tar_len - len(tar_seq)), value=self.tar_padd_idx)
+
+        return src_seq, tar_seq
 
 class Encoder(nn.Module):
     def __init__(self, input_size, embed_size, hidden_size, num_layers, drop):
